cf4731e6e2f663a601c72c237433e70106f5db0b
Colodidac <didaccolominas@gmail.com>
2021-04-01 15:50:15 +0100

    Added model of Movie & Distributor

--- fakefilmsweb/models.py
+++ fakefilmsweb/models.py
@@ -9,5 +9,14 @@ class User(models.Model):
     def __str__(self):
         return self.user_name
 
+class Distributor(models.Model):
+    distributor_name=models.CharField(max_length=30)
 
-    
+    def __str__(self):
+        return self.distributor_name
+
+class Movie(models.Model):
+    movie_name= models.CharField(max_length=30)
+    movie_category= models.CharField(max_length=50)
+    def __str__(self):
+        return self.movie_name
