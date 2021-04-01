be8ba99be9996c8e77b99006674db80282d7da92
alejandroclavera <alejandroclavera@gmail.com>
2021-04-01 17:42:53 +0200

    migrations of all first models version

--- fakefilmsweb/admin.py
+++ fakefilmsweb/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import * 
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Distributor)
+admin.site.register(Movie)
+admin.site.register(Serie)
+admin.site.register(Episode)
 
--- fakefilmsweb/models.py
+++ fakefilmsweb/models.py
@@ -10,14 +10,14 @@ class User(models.Model):
         return self.user_name
 
 class Distributor(models.Model):
-    distributor_name=models.CharField(max_length=30)
-
-     def __str__(self):
+    distributor_name = models.CharField(max_length=30)
+    def __str__(self):
         return self.distributor_name
 
 class Movie(models.Model):
     movie_name= models.CharField(max_length=30)
     movie_category= models.CharField(max_length=50)
+    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.movie_name
@@ -26,7 +26,6 @@ class Serie(models.Model):
     name = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
     seasons = models.IntegerField()
-
     def __str__(self):
         return self.name
 
@@ -35,3 +34,6 @@ class Episode(models.Model):
     season = models.CharField(max_length=30)
     number = models.IntegerField()
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
